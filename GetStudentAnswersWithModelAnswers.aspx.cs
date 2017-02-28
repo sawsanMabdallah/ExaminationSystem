387b7ce5b100087901581174363187152048fa46
eman <eng.eman.elkhouly@gmail.com>
2017-02-28 04:18:13 +0200

    Add Report 6

---
+++ GetStudentAnswersWithModelAnswers.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GetStudentAnswersWithModelAnswers : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+
+
+    protected void Button1_GetAllstudent_Click(object sender, EventArgs e)
+    {
+        Session.Add("Exam_id_forReport", TextBox1_ExamId.Text);
+        Session.Add("Student_id_forReport", TextBox1_StudentId.Text);
+
+        Response.Redirect("StudentAnswersWithModelAnswersReport.aspx");
+    }
+
+    protected void TextBox1_Deptno_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
